a2700577ccec8cfcaf4dc7f4a122ce79da85f00a
Yurik16 <chuksin.yury@gmail.com>
2017-01-16 20:41:50 +0300

    Adding all cheking methods in CalculatorTest

--- chapter_001/src/main/java/ru/job4j/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/Calculator.java
@@ -1,45 +1,51 @@
-package job4j.ru;
+package ru.job4j;
 /**
-* Calculator class
+* Calculator class.
 * @author Yury Chuksin (chuksin.yury@gmail.com)
 * @since  14.01.2017
 */
 
 public class Calculator {
-		private double result;	
 		/**
-		* Add method calculate addition of two arguments
-		* @params first, second inners parametors of that method
+		* @param result stores the result of the methods
+		*/
+		private double result;
+		/**
+		* Add method calculate addition of two arguments.
+		* @param first inner parametor of this method
+		* @param second inner parametor of this method
 		*/
 		public void add(double first, double second) {
 			this.result = first + second;
 		}
 		/**
-		* Substruct method calculate substruction of two arguments
-		* @params first, second inners parametors of that method
+		* Substruct method calculate substruction of two arguments.
+		* @param first inner parametor of this method
+		* @param second inner parametor of this method
 		*/
 		public void substruct(double first, double second) {
 			this.result = first - second;
 		}
 		/**
-		* Div method calculate division of two arguments
-		* @params first, second inners parametors of that method
+		* Div method calculate division of two arguments.
+		* @param first inner parametor of this method
+		* @param second inner parametor of this method
 		*/
 		public void div(double first, double second) {
 			this.result = first / second;
 		}
 		/**
-		* multiple method calculate multiplication of two arguments
-		* @params first, second inners parametors of that method
+		* multiple method calculate multiplication of two arguments.
+		* @param first inner parametor of this method
+		* @param second inner parametor of this method
 		*/
 		public void multiple(double first, double second) {
 			this.result = first * second;
 		}
 		/**
-		* GetResult method calculate division of two arguments
-		* @params args
+		* @return getResult() getter that returns value of private parametor "result".
 		*/
 		public double getResult() {
 			return result;
 		}
-} 
+}
--- chapter_001/src/test/java/ru/job4j/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculatorTest.java
@@ -1,36 +1,80 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
 /**
 * Class CalculatorTest.
 * @author Yury Chuksin (chuksin.yury@gmail.com)
 * @version $Id@
 * @since 15.01.2017
 */
-
 public class CalculatorTest {
+	/**
+	* @param testArgOne first value for making this test
+	*/
+	private final double testArgOne = 6;
+	/**
+	* @param testArgTwo second value for making this test
+	*/
+	private final double tesrArgTwo = 3;
+	/**
+	* Making new empty object.
+	* @see Calculator
+	*/
+	private Calculator calc = new Calculator();
 	/**
 	* Test add.
 	*/
 	@Test
-	double private testArg1 = 6;
-	double private tesrArg2 = 3;
-	
-	Calculator calc = new Calculator;
-	
 	/**
 	* whenDoAddMethodThanGetAddition method compare result of additional
-	* @params args
 	*/
 	public void whenDoAddMethodThanGetAddition() {
-		double testRes = this.testArg1 + this.tesrArg2;
-		double testValues = calc.add(this.testArg1, this.tesrArg2);
+		/**
+		* @param testRes correct result of cheking method
+		*/
+		double testRes = this.testArgOne + this.tesrArgTwo;
+		/**
+		* @param testValues geting result from cheking method
+		*/
+		double testValues = this.calc.add(this.testArg1, this.tesrArg2);
 		assertThat(testRes, is(testValues));
 	}
-	
+	/**
+	* Test add.
+	*/
+	@Test
+	/**
+	* whenDoSubstructMethodThanGetSubstration method compare result of substraction
+	*/
+	public void whenDoSubstructMethodThanGetSubstration() {
+		double testRes = this.testArgOne - this.testArgTwo;
+		double testValues = this.calc.add(this.testArgOne, this.tesrArgTwo);
+		assertThat(testRes, is(testValues));
+	}
+	/**
+	* Test add.
+	*/
+	@Test
+	/**
+	* whenDoDivMethodThanGetSubstration method compare result of division
+	*/
+	public void whenDoDivMethodThanGetDivision() {
+		double testRes = this.testArgOne / this.testArgTwo;
+		double testValues = this.calc.add(this.testArgOne, this.tesrArgTwo);
+		assertThat(testRes, is(testValues));
+	}
+	/**
+	* Test add.
+	*/
+	@Test
+	/**
+	* whenDoMultipleMethodThanGetMultiplication method compare result of multiplication
+	*/
+	public void whenDoMultipleMethodThanGetMultiplication() {
+		double testRes = this.testArgOne * this.testArgTwo;
+		double testValues = this.calc.add(this.testArgOne, this.tesrArgTwo);
+		assertThat(testRes, is(testValues));
+	}
 }
