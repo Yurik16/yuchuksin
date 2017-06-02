5a5462f3446193868503aaf740fb44f6c8660ec4
Yurik16 <chuksin.yury@gmail.com>
2017-06-02 23:41:07 +0300

    Chap5 Collections Lite Sort

---
+++ chapter_005/src/main/java/ru/job4j/collections/Sort/Comparable.java
@@ -0,0 +1,10 @@
+package ru.job4j.collections.Sort;
+
+/**
+ * Main class.
+ *
+ * @author Yury Chuksin (chuksin.yury@gmail.com)
+ * @since 02.06.2017.
+ */
+public interface Comparable {
+}
---
+++ chapter_005/src/main/java/ru/job4j/collections/Sort/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Main class.
+ *
+ * @author Yury Chuksin (chuksin.yury@gmail.com)
+ * @since 02.06.2017.
+ */
+package ru.job4j.collections.Sort;
